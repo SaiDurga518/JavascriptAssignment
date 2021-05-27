b1a19eb4447babe66d0d0c263e77247914148088
SaiDurga518 <84706977+SaiDurga518@users.noreply.github.com>
2021-05-27 21:08:51 +0530

    Add files via upload

--- vowelscount.js
+++ vowelscount.js
@@ -5,10 +5,13 @@ function vowelscount(str) {
      return count;
 }
 
-string = prompt('Enter a string: ');
+string = "Hey This is Skillsanta JS Training";//prompt('Enter a string: ');
 
 result = vowelscount(string);
 
 //console.log(result);
-
+console.log(vowelscount("Hey This is Skillsanta JS Training"));
+//document.write(vowelscount("Hey This is Skillsanta JS Training",+ result+ "<br/>"));
 document.write("the Count of Vowels in string is  ",+ result);
+//document.write(vowelscount("Hey This is Skillsanta JS Training"));
+
